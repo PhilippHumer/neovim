dd7503b8bb671e1ccd8789c29393820b5088ab53
Philipp Humer <philipp.humer@gehealthcare.com>
2025-04-07 15:13:10 +0200

    Add lemminx language server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,14 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "jdtls", "phpactor" }
+local servers = { 
+  "html", 
+  "cssls", 
+  "clangd", 
+  "jdtls", 
+  "phpactor", 
+  "lemminx"
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
