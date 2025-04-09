8a89964a4519fa5aba9f98d7bab095a6659139d1
Philipp Humer <philipp.humer@gmx.at>
2025-04-09 17:39:04 +0200

    set tab and shiftstop option to 4

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,10 @@
 require "nvchad.options"
 
 -- add yours here!
+require('dap.ext.vscode').load_launchjs(nil, {})
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
